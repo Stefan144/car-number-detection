f331599dcdf4f177b89f44055b2a1bae85baa319
Stefan <futurelifestefan@gmail.com>
2020-03-30 23:04:03 +0300

    worker added

---
+++ worker/ocr_stream.py
@@ -0,0 +1,50 @@
+import logging
+from threading import Thread
+from cnd.ocr.transforms import get_transforms
+
+from cnd.ocr.predictor import Predictor
+from worker.state import State
+from worker.video_reader import VideoReader
+
+
+class OcrStream:
+    def __init__(self, name, state: State, video_reader: VideoReader, model_path):
+        self.name = name
+        self.logger = logging.getLogger(self.name)
+        self.transform = get_transforms([32, 96])
+        self.state = state
+        self.video_reader = video_reader
+        self.ocr_thread = None
+        self.model_path = model_path
+        self.predictor = Predictor(self.model_path)
+        self.stopped = False
+        self.logger.info("Create OcrStream")
+
+    def _ocr_loop(self):
+        try:
+            while True:
+                if self.stopped:
+                    return
+                frame = self.video_reader.read()
+                self.state.frame = frame
+                frame = self.transform(frame)[None, :, :, :]
+                pred = self.predictor.predict(frame)
+                #print(pred)
+                self.state.text = pred
+        except Exception as e:
+            self.logger.exception(e)
+            self.state.exit_event.set()
+
+    def _start_ocr(self):
+        self.ocr_thread = Thread(target=self._ocr_loop)
+        self.ocr_thread.start()
+
+    def start(self):
+        self._start_ocr()
+        self.logger.info("Start OcrStream")
+
+    def stop(self):
+        self.stopped = True
+        if self.ocr_thread is not None:
+            self.ocr_thread.join()
+        self.logger.info("Stop OcrStream")
